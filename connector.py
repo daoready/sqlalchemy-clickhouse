ef9936399487fce9d74dbaa86be3f28baa502d1b
Daoready <info@daoready.com>
2018-03-15 11:32:15 +0300

    readonly option added

--- connector.py
+++ connector.py
@@ -114,12 +114,13 @@ class Connection(Database):
     """
         These objects are small stateless factories for cursors, which do all the real work.
     """
-    def __init__(self, db_name, db_url='http://localhost:8123/', username=None, password=None):
-        super(Connection, self).__init__(db_name, db_url, username, password)
+    def __init__(self, db_name, db_url='http://localhost:8123/', username=None, password=None, readonly=False):
+        super(Connection, self).__init__(db_name, db_url, username, password, readonly)
         self.db_name = db_name
         self.db_url = db_url
         self.username = username
         self.password = password
+        self.readonly = readonly
 
     def close(self):
         pass
